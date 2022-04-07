1699a44b645240a055f49e36802755ba4c154d28
NEERAJ <6620913@gmail.com>
2022-04-07 16:42:08 +0530

    some imports in training.py

---
+++ chatbot/training.py
@@ -0,0 +1,11 @@
+from copyreg import pickle
+import random 
+import json
+import pickle
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tenserflow.keras.models import Sequential
+from tenserflow.keras.layers import Dense, Activation,Dropout
+from tenserflow.keras.optimizers import SGD
